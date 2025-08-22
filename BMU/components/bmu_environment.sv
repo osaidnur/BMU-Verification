6a439b549dca01af29f5d1afca3a52aa6483edd2
Osaid Nur <1210733@student.birzeit.edu>
2025-08-22 14:39:29 +0300

    nothing important

--- BMU/components/bmu_environment.sv
+++ BMU/components/bmu_environment.sv
@@ -1,22 +1,25 @@
 class bmu_environment extends uvm_env; 
+
 `uvm_component_utils(bmu_environment)
+
 bmu_agent agent;
 bmu_scoreboard scoreboard;
 bmu_subscriber subscriber;
 
 function new(string name,uvm_component parent); 
-super.new(name,parent); 
+    super.new(name,parent);
 endfunction
 
 function void build_phase(uvm_phase phase); 
-super.build_phase(phase); 
-agent = bmu_agent::type_id::create("agent",this);
-scoreboard = bmu_scoreboard::type_id::create("scoreboard",this); 
-subscriber = bmu_subscriber::type_id::create("subscriber",this); 
+    super.build_phase(phase); 
+    agent = bmu_agent::type_id::create("agent",this);
+    scoreboard = bmu_scoreboard::type_id::create("scoreboard",this); 
+    subscriber = bmu_subscriber::type_id::create("subscriber",this); 
 endfunction 
 
 function void connect_phase(uvm_phase phase);
-agent.monitor.port.connect(scoreboard.exp);
-agent.monitor.port.connect(subscriber.analysis_export);
+    agent.monitor.port.connect(scoreboard.exp);
+    agent.monitor.port.connect(subscriber.analysis_export);
 endfunction
+
 endclass: bmu_environment
