963d201091edf133b443e5c8e2aea56577f5ccbb
Osaid Nur <1210733@student.birzeit.edu>
2025-08-23 19:13:23 +0300

    add "AND" test

---
+++ BMU/tests/bmu_and_test.sv
@@ -0,0 +1,25 @@
+class bmu_and_test extends uvm_test;
+`uvm_component_utils(bmu_and_test)
+
+bmu_environment env;
+bmu_and_sequence bmu_sequence;
+
+function new(string name,uvm_component parent);
+    super.new(name,parent);
+endfunction
+
+function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = bmu_environment::type_id::create("environment",this);
+endfunction
+
+task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    bmu_sequence = bmu_and_sequence::type_id::create("bmu_and_sequence");
+    repeat(10) begin
+        bmu_sequence.start(env.agent.sequencer);
+    end
+    phase.drop_objection(this);
+    `uvm_info(get_type_name, "End of testcase", UVM_LOW);
+endtask
+endclass
