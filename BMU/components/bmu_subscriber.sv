4260d2d27e3573b10782c85598a2d74f97215c87
Osaid Nur <1210733@student.birzeit.edu>
2025-08-23 15:20:48 +0300

    initial set of the subscriber

--- BMU/components/bmu_subscriber.sv
+++ BMU/components/bmu_subscriber.sv
@@ -1,32 +1,37 @@
-class alu_subscriber extends uvm_subscriber #(alu_sequence_item); 
-`uvm_component_utils(alu_subscriber) 
+class bmu_subscriber extends uvm_subscriber #(bmu_sequence_item); 
+`uvm_component_utils(bmu_subscriber) 
 
-alu_sequence_item sub; 
+bmu_sequence_item sub; 
 
-covergroup aluCoverage;
-    A: coverpoint sub.A; 
-    B: coverpoint sub.B; 
-    Opcode: coverpoint sub.opcode ; 
-    Result: coverpoint sub.result; 
+covergroup bmuCoverage;
+    A: coverpoint sub.a_in; 
+    B: coverpoint sub.b_in; 
+    AP_ADD: coverpoint sub.ap.add; 
+    AP_SUB: coverpoint sub.ap.sub; 
+    AP_LAND: coverpoint sub.ap.land; 
+    AP_LXOR: coverpoint sub.ap.lxor; 
+    Result: coverpoint sub.result_ff; 
     Error: coverpoint sub.error; 
 endgroup
 
 function new(string name,uvm_component parent); 
     super.new(name,parent); 
-    aluCoverage = new(); 
+    bmuCoverage = new(); 
     sub = new(); 
 endfunction
-function void write (alu_sequence_item t);
-    sub.A = t.A; 
-    sub.B = t.B; 
-    sub.opcode = t.opcode; 
-    sub.result = t.result; 
+
+function void write (bmu_sequence_item t);
+    sub.a_in = t.a_in; 
+    sub.b_in = t.b_in; 
+    sub.ap = t.ap; 
+    sub.result_ff = t.result_ff; 
     sub.error = t.error; 
-    aluCoverage.sample();
+    bmuCoverage.sample();
 endfunction
+
 function void report_phase(uvm_phase phase); 
     super.report_phase(phase); 
     `uvm_info(get_type_name,  
-    $sformatf("coverage: %d",aluCoverage.get_coverage()), UVM_HIGH); 
+    $sformatf("coverage: %d",bmuCoverage.get_coverage()), UVM_HIGH); 
 endfunction
 endclass
