1b25a63bf21864fd5a92eee0eff5cebb1f8f6ea0
Osaid Nur <1210733@student.birzeit.edu>
2025-08-23 22:08:47 +0300

    Add test classes for all features

---
+++ BMU/tests/bmu_sra_test.sv
@@ -0,0 +1,23 @@
+class bmu_sra_test extends uvm_test;
+`uvm_component_utils(bmu_sra_test)
+
+bmu_environment env;
+bmu_sra_sequence bmu_sequence;
+
+function new(string name,uvm_component parent);
+    super.new(name,parent);
+endfunction
+
+function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = bmu_environment::type_id::create("environment",this);
+endfunction
+
+task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    bmu_sequence = bmu_sra_sequence::type_id::create("bmu_sra_sequence");
+    bmu_sequence.start(env.agent.sequencer);
+    phase.drop_objection(this);
+    `uvm_info(get_type_name, "========= End of SRA Test =========", UVM_LOW);
+endtask
+endclass
