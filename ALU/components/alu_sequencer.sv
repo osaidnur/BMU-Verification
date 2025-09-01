933b4f3fb72653b138012ff157a8a9fa93ec96aa
Osaid Nur <1210733@student.birzeit.edu>
2025-09-01 22:56:21 +0300

    delete ALU project

--- ALU/components/alu_sequencer.sv
+++
@@ -1,9 +0,0 @@
-class alu_sequencer extends uvm_sequencer #(alu_sequence_item);
-
-`uvm_component_utils(alu_sequencer)
-
-function new(string name = "alu_sequencer", uvm_component parent);
-  super.new(name, parent);
-endfunction: new
-
-endclass: alu_sequencer
